faf1b2ba9e40798ec64e490e8f33fec772079947
dinodesmond <dinodesmond8@gmail.com>
2023-10-04 15:48:41 +0000

    creates pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import signIn from './pages/signIn';
+import signUp from './pages/signUp';
+import profile from './pages/profile';
 
 export default function App() {
-  return (
-    <h1 className='text-3xl font-bold underline text-red-600'>Hello world</h1>
-  )
+  return <BrowserRouter>
+      <Routes>
+      <Route path='/' element = {<Home /> }/>
+      <Route path='/about' element = {<About /> }/>
+      <Route path='/sign-up' element = {<signUp /> }/>
+      <Route path='/sign-in' element = {<signIn /> }/>
+      <Route path='/profile' element = {<profile /> }/>
+    </Routes>
+    </BrowserRouter>
 }
