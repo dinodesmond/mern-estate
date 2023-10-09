4dfc89098d24eeadeca135a50a0df873bcb64317
dinodesmond <dinodesmond8@gmail.com>
2023-10-09 16:43:33 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();  
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to Database!');
+}).catch((err) =>{
+    console.log(err)
+});
 
 const app = express();
 
