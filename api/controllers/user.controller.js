14cab4ca78c541d05153589e9983c1ceb8ddab11
dinodesmond <dinodesmond8@gmail.com>
2023-10-11 08:37:23 +0000

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+
+const test = ('/test', (req, res) =>{
+    res.json({message: 'API is working!'});
+});
+
+module.exports = test;
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();  
+const router = require('./routes/user.route')
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to Database!');
@@ -9,6 +10,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
+app.use('/api/user', router)
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000...');
