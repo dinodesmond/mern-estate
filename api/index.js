0ef878ccf563e8630d40e24b7836db2164572b34
dinodesmond <dinodesmond8@gmail.com>
2023-10-12 15:25:40 +0000

    creates sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();  
 const router = require('./routes/user.route')
+const authRouter = require('./routes/auth.route')
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to Database!');
@@ -10,7 +11,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
-app.use('/api/user', router)
+app.use(express.json());
+app.use('/api/user', router);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000...');
